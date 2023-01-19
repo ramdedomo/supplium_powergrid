8ca6f3c287dc5141a372f655485a29e448d1e7f1
Ram Dedomo <ramdedomo@gmail.com>
2023-01-19 19:24:19 +0800

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,19 +13,9 @@ module.exports = {
     './vendor/wireui/wireui/src/View/**/*.php',
     './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     './vendor/wire-elements/modal/src/ModalComponent.php',
-  ],
-  purge: [
-    './vendor/wire-elements/modal/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-    './vendor/wireui/wireui/resources/**/*.blade.php',
-    './vendor/wireui/wireui/ts/**/*.ts',
-    './vendor/wireui/wireui/src/View/**/*.php',
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './vendor/wire-elements/modal/src/ModalComponent.php',
+    './app/Http/Livewire/**/*Table.php',
+    './vendor/power-components/livewire-powergrid/resources/views/**/*.php',
+    './vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php'
   ],
   options: {
     safelist: [
